de5978b705f38e4aebdccbf85e71256437b28b58
Inje <byeo630@gmail.com>
2018-11-27 16:48:18 -0800

    Remove `export = CopyToClipboard`
    # Changelog
    - Remove `line 12`
    ```
    export = CopyToClipboard;
    ```
    Fixes type error below:
    ```
    Type error: Module '"/node_modules/@types/react-copy-to-clipboard/index"' has no exported member 'CopyToClipboard'.  TS2305
    ```
    Importing `CopyToClipboard` gets imported from `node_modules/@types/react-copy-to-clipboard` instead of `node_modules/react-copy-to-clipboard` because of the `CopyToClipboard` export from the `@types/react-copy-to-clipboard`.

--- types/react-copy-to-clipboard/index.d.ts
+++ types/react-copy-to-clipboard/index.d.ts
@@ -2,14 +2,13 @@
 // Project: https://github.com/nkbt/react-copy-to-clipboard
 // Definitions by: Meno Abels <https://github.com/mabels>
 //                 Bernabe <https://github.com/BernabeFelix>
+//                 Inje Yeo <https://github.com/inje>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
 import * as React from "react";
 
 export as namespace CopyToClipboard;
-
-export = CopyToClipboard;
 
 declare namespace CopyToClipboard {
   interface Options {
